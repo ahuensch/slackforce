5dc2a042fa2c9e734690cbde44d391c8e6b4ed1a
Anna Huensch <anna.huensch@gmail.com>
2016-01-22 23:36:31 -0600

    Double Checks Team_ID

--- server.js
+++ server.js
@@ -21,5 +21,9 @@ app.listen(app.get('port'), function () {
     auth.login();
 });
 function execute (req, resp){
+  if (req.body.team_id !== process.env.TEAM_ID){
+    res.send("Unauthorized Access");
+    return;
+  }
   resp.json({text:"it works " + req.body.team_id + " " + req.body.token + " " + req.body.command + " " + req.body.response_url + " " + req.body.channel_name + " " + req.body.team_domain});
 }
