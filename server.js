d82eca9509d8c04f0a580d6728a6e92ba7148772
Anna Huensch <anna.huensch@gmail.com>
2016-01-23 15:38:41 -0600

    CleanUp

--- server.js
+++ server.js
@@ -1,38 +1,24 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+
 var requireDir = require('require-dir');
 var commands = requireDir('./commands');
 
-var express = require('express'),
-    bodyParser = require('body-parser'),
-    auth = require('./modules/auth'),
-    contact = require('./modules/contact'),
-    opportunity = require('./modules/opportunity'),
-    _case = require('./modules/case'),
-    app = express();
-
 app.set('port', process.env.PORT || 5000);
-
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.post('/pipeline', opportunity.execute);
-app.post('/contact', contact.execute);
-app.post('/case', _case.execute);
-app.post('/', execute);
-app.listen(app.get('port'), function () {
-    console.log('Express server listening on port ' + app.get('port'));
-    auth.login();
-});
-function execute (req, resp){
+app.post('/', function (req,resp){
   if (req.body.team_id !== process.env.TEAM_ID){
     resp.send(401, "Unauthorized Access");
     return;
   }
-  var result = "";
-  for (var prop in commands){
-    result += prop + " ";
-  }
+
   var name = req.body.command.replace('/','');
   commands[name](req,resp);
-  //[req.body.command.replace('/','')].module(req,resp);
-  //commands[req.body.command.replace('/','')](req,resp);
-  //resp.json({text:"it works " + req.body.team_id + " " + req.body.token + " " + req.body.command + " " + req.body.response_url + " " + req.body.channel_name + " " + req.body.team_domain});
-}
+});
+
+app.listen(app.get('port'), function () {
+    console.log('Express server listening on port ' + app.get('port'));
+    auth.login();
+});
