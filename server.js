6b489d2ade8f52adc03a9c549bd2933f15ca5c2d
Anna Huensch <anna.huensch@gmail.com>
2016-01-23 16:32:04 -0600

    Tests Contact

--- server.js
+++ server.js
@@ -5,10 +5,11 @@ var app = express();
 var auth = require('./modules/auth');
 var requireDir = require('require-dir');
 var commands = requireDir('./commands');
+var contact = require('./modules/contact');
 
 app.set('port', process.env.PORT || 5000);
 app.use(bodyParser.urlencoded({extended: true}));
-
+app.post('/contact', contact.execute);
 app.post('/', function (req,resp){
   if (req.body.team_id !== process.env.TEAM_ID){
     resp.send(401, "Unauthorized Access");
