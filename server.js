e982008fb67c4905400ea506f674200a106db769
Anna Huensch <anna.huensch@gmail.com>
2016-01-23 00:16:09 -0600

    Fix

--- server.js
+++ server.js
@@ -26,6 +26,6 @@ function execute (req, resp){
     resp.send(401, "Unauthorized Access");
     return;
   }
-  commands[req.body.command.replace('/','')](req,resp);
+  commands[req.body.command.replace('/','')].module(req,resp);
   //resp.json({text:"it works " + req.body.team_id + " " + req.body.token + " " + req.body.command + " " + req.body.response_url + " " + req.body.channel_name + " " + req.body.team_domain});
 }
