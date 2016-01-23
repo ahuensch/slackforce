a133618e2422e75736328c467c44934460fb70ef
Anna Huensch <anna.huensch@gmail.com>
2016-01-23 00:21:01 -0600

    More Fixes

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var requireDir = requrie('require-dir');
-var commands = requireDir('./commands');
+//var commands = requireDir('./commands');
 
 var express = require('express'),
     bodyParser = require('body-parser'),
@@ -26,7 +26,7 @@ function execute (req, resp){
     resp.send(401, "Unauthorized Access");
     return;
   }
-resp.send(commands)
+resp.send("blah")
   //commands[req.body.command.replace('/','')].module(req,resp);
   //resp.json({text:"it works " + req.body.team_id + " " + req.body.token + " " + req.body.command + " " + req.body.response_url + " " + req.body.channel_name + " " + req.body.team_domain});
 }
