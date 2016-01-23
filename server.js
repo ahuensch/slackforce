f229a52a21818c776f5511ff3abf878bbbbc660d
Anna Huensch <anna.huensch@gmail.com>
2016-01-22 21:18:15 -0600

    Adds database to POST method

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var express = require('express'),
     contact = require('./modules/contact'),
     opportunity = require('./modules/opportunity'),
     _case = require('./modules/case'),
+    database = require('./modules/database'),
     app = express();
 
 app.set('port', process.env.PORT || 5000);
@@ -13,6 +14,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.post('/pipeline', opportunity.execute);
 app.post('/contact', contact.execute);
 app.post('/case', _case.execute);
+app.post('/db', database.execute);
 
 app.listen(app.get('port'), function () {
     console.log('Express server listening on port ' + app.get('port'));
