07d7cda60cde6ecaba252fe82db95fee8aff54df
Anna Huensch <anna.huensch@gmail.com>
2016-01-27 21:45:32 -0600

    Adds command-runner

--- commands/command1.js
+++ commands/command1.js
@@ -1,28 +1,26 @@
-var org = require('./auth').org;
+module.exports = {
+  title: function(searchTerm) {
+    return "Contacts matching: " + searchTerm;
+  },
 
-module.exports = function(req, res){
-
-  var q = "SELECT Id, Name, Phone, MobilePhone, Email FROM Contact WHERE Name LIKE '%" + req.body.text + "%' LIMIT 5";
-  org.query({query: q}, function(err, resp) {
-      if (err) {
-          console.error(err);
-          res.send("An error as occurred");
-          return;
-      }
-      if (resp.records && resp.records.length>0) {
-          var contacts = resp.records;
-          var attachments = [];
-          contacts.forEach(function(contact) {
-              var fields = [];
-              fields.push({title: "Name", value: contact.get("Name"), short:true});
-              fields.push({title: "Phone", value: contact.get("Phone"), short:true});
-              fields.push({title: "Mobile", value: contact.get("MobilePhone"), short:true});
-              fields.push({title: "Email", value: contact.get("Email"), short:true});
-              attachments.push({color: "#009cdb", fields: fields});
-          });
-          res.json({text: "Contacts matching '" + req.body.text + "':", attachments: attachments});
-      } else {
-          res.send("No records");
-      }
-  });
+  query: function(searchTerm) {
+    return
+      "SELECT Id, Name, Phone, MobilePhone, Email"
+      + " FROM Contact "
+      + " WHERE Name LIKE '%" + searchTerm + "%'"
+      + " LIMIT 5";
+  },
+  
+  attachments: function(contacts) {
+    var attachments = [];
+    contacts.forEach(function(contact) {
+        var fields = [];
+        fields.push({title: "Name", value: contact.get("Name"), short:true});
+        fields.push({title: "Phone", value: contact.get("Phone"), short:true});
+        fields.push({title: "Mobile", value: contact.get("MobilePhone"), short:true});
+        fields.push({title: "Email", value: contact.get("Email"), short:true});
+        attachments.push({color: "#009cdb", fields: fields});
+    });
+    return attachments;
+  }
 }
--- server.js
+++ server.js
@@ -2,24 +2,32 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
 
-var auth = require('./modules/auth');
-var requireDir = require('require-dir');
-var commands = requireDir('./commands');
-var contact = require('./modules/contact');
+// Setup App
+var auth = require('./commands/auth');
+var run = require('./commands/command-runner');
 
 app.set('port', process.env.PORT || 5000);
 app.use(bodyParser.urlencoded({extended: true}));
-app.post('/contact', contact.execute);
+
+
+// Setup Commands
+var requireDir = require('require-dir');
+var commands = requireDir('./commands');
+
 app.post('/', function (req,resp){
   if (req.body.team_id !== process.env.TEAM_ID){
     resp.send(401, "Unauthorized Access");
     return;
   }
 
+  // Execute specified command
   var name = req.body.command.replace('/','');
-  commands[name](req,resp);
+  var command = commands[name]
+  run(req, resp, command);
 });
 
+
+// Run Server
 app.listen(app.get('port'), function () {
     console.log('Express server listening on port ' + app.get('port'));
     auth.login();
