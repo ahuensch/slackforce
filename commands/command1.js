840a6c2b49a693fabe98ae1259e47c5586211af4
Anna Huensch <anna.huensch@gmail.com>
2016-01-27 20:42:25 -0600

    Revised RESP

--- commands/command1.js
+++ commands/command1.js
@@ -4,7 +4,7 @@ module.exports = function(req, resp){
 
   var q = "SELECT Id, Org_PIN_DB__c, SQL_Server_Name__c, SQL_Database_Name__c ,Name, Account_Name__c FROM Application_Database__c LIMIT 5";
 
-  org.query({query: q}, function(err, resp) {
+  org.query({query: q}, function(err, result) {
     resp.send("Query Executed!");
   });
 
